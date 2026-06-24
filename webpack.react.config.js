1bb12d9fcd9bf0a8f8eec50bc020011152d858e5
yururi-nonbiry <yururi.nonbiry@gmail.com>
2026-06-24 20:10:32 +0900

    feat: add favicon to webpack configuration and include asset file

--- webpack.react.config.js
+++ webpack.react.config.js
@@ -26,6 +26,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html',
+            favicon: './src/assets/favicon.png',
         }),
     ],
     resolve: {
